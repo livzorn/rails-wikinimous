06d2ee2917794e0e253b3bd53c43a0a20e5b7240
Olivia Zorn <livzorn@gmail.com>
2020-11-10 18:48:09 +0000

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  title = Faker::Quote.famous_last_words
+  content = Faker::Quote.most_interesting_man_in_the_world
+  Article.create(title: title, content: content)
+end
